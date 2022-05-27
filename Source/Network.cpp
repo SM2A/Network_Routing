ea7441b43bbd5f7c0810ca3fa197dbebe7690f85
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-05-27 22:31:04 +0430

    Format print

--- Source/Network.cpp
+++ Source/Network.cpp
@@ -1,7 +1,12 @@
 #include "Network.h"
 #include <iostream>
+#include <iomanip>
 
-#define SAME_ERROR "Source and Destination cannot be the same"
+#define NO_LINK -1
+#define SAME_NODE 0
+#define NUM_WIDTH 5
+#define SEPARATOR ' '
+#define SAME_NODE_ERROR "Source and Destination cannot be the same"
 
 using namespace std;
 
@@ -13,28 +18,40 @@ Network *Network::getInstance() {
 }
 
 void Network::show() {
-    for (auto &i : links) {
-        for (int j : i) {
-            cout << j << " ";
-        }
+
+    cout << left << setw(NUM_WIDTH) << setfill(SEPARATOR) << "      ";
+
+    for (int i = 0; i < links.size(); ++i) cout << left << setw(NUM_WIDTH) << setfill(SEPARATOR) << i;
+    cout << endl;
+
+    for (int i = 0; i < ((links.size() * NUM_WIDTH) + NUM_WIDTH); ++i) cout <<'_';
+    cout << endl;
+
+    for (int i = 0; i < links.size(); ++i) {
+        cout << left << setw(NUM_WIDTH) << setfill(SEPARATOR) << i << '|';
+        for (int j : links[i]) cout << left << setw(NUM_WIDTH) << setfill(SEPARATOR) << j;
         cout << endl;
     }
 }
 
 void Network::addLink(int source, int destination, int cost) {
 
+    // TODO add new node to node list
+
     if (source == destination) {
-        cerr << SAME_ERROR << endl;
+        cerr << SAME_NODE_ERROR << endl;
         return;
     }
 
     for (int i = 0; links.size() < max(source, destination); ++i) {
         vector<int> row;
-        for (int j = 0; row.size() < max(source, destination); ++j) row.push_back(-1);
+        for (int j = 0; row.size() < max(source, destination); ++j) row.push_back(NO_LINK);
         links.push_back(row);
     }
 
-    for (auto &link : links) for (int j = 0; link.size() < max(source, destination); ++j) link.push_back(-1);
+    for (auto &link : links) for (int j = 0; link.size() < max(source, destination); ++j) link.push_back(NO_LINK);
+
+    for (int i = 0; i < links.size(); ++i) links[i][i] = SAME_NODE;
 
     source -= 1;
     destination -= 1;
@@ -46,7 +63,7 @@ void Network::addLink(int source, int destination, int cost) {
 void Network::modifyLink(int source, int destination, int cost) {
 
     if (source == destination) {
-        cerr << SAME_ERROR << endl;
+        cerr << SAME_NODE_ERROR << endl;
         return;
     }
 
