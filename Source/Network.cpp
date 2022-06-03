f02bd65da99d9127cc5255806d00edfaf1f9744e
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-06-03 12:06:26 +0430

    Final ✔

--- Source/Network.cpp
+++ Source/Network.cpp
@@ -1,8 +1,8 @@
-#include <chrono>
 #include <iomanip>
 #include <iostream>
 #include "Network.h"
 #include <algorithm>
+//#include <chrono>
 
 #define INF 99999
 #define NO_LINK -1
@@ -158,7 +158,7 @@ void Network::dijkstra(int src) {
     vector<int> visited(links.size(), 0);
     vector<int> lastCost = copyDistance(node->distance);
 
-    auto begin = chrono::high_resolution_clock::now();
+    /*auto begin = chrono::high_resolution_clock::now();*/
 
     for (int j = 0; j < links.size() - 1; j++) {
 
@@ -183,7 +183,7 @@ void Network::dijkstra(int src) {
             }
         }
 
-        /*cout << "Iter " << j + 1 << ":" << endl;
+        cout << "Iter " << j + 1 << ":" << endl;
 
         cout << left << setw(NUM_WIDTH * 2) << setfill(SEPARATOR) << "Dest";
 
@@ -199,18 +199,18 @@ void Network::dijkstra(int src) {
             if (node->distance[k] == INF) cout << left << setw(NUM_WIDTH * 2) << setfill(SEPARATOR) << NO_LINK;
             else cout << left << setw(NUM_WIDTH * 2) << setfill(SEPARATOR) << node->distance[k];
         }
-        cout << endl;*/
+        cout << endl;
 
         /*if (compareCost(lastCost, node->distance)) break;*/ // To end algorithm when two iterations are the same
         /*lastCost = copyDistance(node->distance);*/ // To end algorithm when two iterations are the same
 
-        /*for (int k = 0; k < (links.size() * (NUM_WIDTH * 2) + NUM_WIDTH); ++k) cout << '_';
-        cout << endl << endl;*/
+        for (int k = 0; k < (links.size() * (NUM_WIDTH * 2) + NUM_WIDTH); ++k) cout << '_';
+        cout << endl << endl;
     }
 
-    auto end = chrono::high_resolution_clock::now();
+    /*auto end = chrono::high_resolution_clock::now();*/
 
-    /*cout << "Path: [s]->[d]   Min-Cost   Shortest Path" << endl;
+    cout << "Path: [s]->[d]   Min-Cost   Shortest Path" << endl;
     cout << "      ---------  ---------  --------------" << endl;
     for (int k = 0; k < links.size(); k++) {
         if (k == src) continue;
@@ -222,10 +222,10 @@ void Network::dijkstra(int src) {
 
         recvPrintPath(node->prev, k);
         cout << endl;
-    }*/
+    }
 
-    auto duration = chrono::duration_cast<chrono::nanoseconds>(end - begin);
-    cout << duration.count() << " nanosecond" << endl;
+    /*auto duration = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    cout << duration.count() << " nanosecond" << endl;*/
 
     cout << endl;
 
@@ -276,7 +276,7 @@ void Network::bellmanFord(int src) {
 
     node->distance[src] = SAME_NODE;
 
-    auto begin = chrono::high_resolution_clock::now();
+    /*auto begin = chrono::high_resolution_clock::now();*/
 
     for (int i = 0; i < links.size(); i++) {
         for (auto &edge : edges) {
@@ -290,9 +290,9 @@ void Network::bellmanFord(int src) {
         }
     }
 
-    auto end = chrono::high_resolution_clock::now();
+    /*auto end = chrono::high_resolution_clock::now();*/
 
-    /*cout << "Dest    Next Hop    Dist    Shortest Path" << endl;
+    cout << "Dest    Next Hop    Dist    Shortest Path" << endl;
     cout << "-----------------------------------------" << endl;
     for (int k = 0; k < links.size(); k++) {
         cout << k + 1
@@ -301,10 +301,10 @@ void Network::bellmanFord(int src) {
              << "        [";
         recvPrintPath(node->path, k, src);
         cout << "]" << endl;
-    }*/
+    }
 
-    auto duration = chrono::duration_cast<chrono::nanoseconds>(end - begin);
-    cout << duration.count() << " nanosecond" << endl;
+    /*auto duration = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    cout << duration.count() << " nanosecond" << endl;*/
 
     cout << endl;
 
